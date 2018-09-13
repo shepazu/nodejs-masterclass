dfc7754c2fe816d1db318245ceb651baf63f1b0a
Doug Schepers <doug@schepers.cc>
2018-09-13 16:14:09 -0400

    Logging to the Console

--- uup/lib/workers.js
+++ uup/lib/workers.js
@@ -3,19 +3,21 @@
 *
 */
 
-// console.log("Uup?");
-
 // Dependencies
 const path = require(`path`);
 const fs = require(`fs`);
 const http = require(`http`);
 const https = require(`https`);
 const url = require(`url`);
+const util = require(`util`);
 const helpers = require(`./helpers`);
 const _data = require(`./data`);
 const _logs = require(`./logs`);
 // const config = require(`./config`);
 // const handlers = require(`./handlers`);
+
+// Create custom debug logger
+const debug = util.debuglog('uup_workers');
 
 // Instantiating the workers module object
 const workers = {};
@@ -32,44 +34,15 @@ workers.gatherAllChecks = () => {
             // Pass it to the check validator, and let that function continue or log errors as needed
             workers.validateCheckData(originalCheckData);
           } else {
-            console.log(`Error: could not read one of the checks' data: ${check}`);
+            debug(`Error: could not read one of the checks' data: ${check}`);
           }
         });
       });
     } else {
-      console.log(`Error: could not find any checks to process`);
+      debug(`Error: could not find any checks to process`);
     }
   });
 };
-
-
-// // Sanity-check the check data
-// workers.validateCheckData = (originalCheckData) => {
-//   originalCheckData = typeof(originalCheckData) ===`object` && originalCheckData !== null ? originalCheckData : {};
-//   originalCheckData.id = typeof(originalCheckData.id) ===`string` && originalCheckData.id.trim().length === 20 ? originalCheckData.id.trim() : null;
-//   originalCheckData.userPhone = typeof(originalCheckData.userPhone) ===`string` && originalCheckData.userPhone.trim().length === 10 ? originalCheckData.userPhone.trim() : null;
-//   originalCheckData.protocol = typeof(originalCheckData.protocol) ===`string` && [`http`, `https`].indexOf(originalCheckData.protocol) > -1 ? originalCheckData.protocol : null;
-//   originalCheckData.url = typeof(originalCheckData.url) ===`string` && originalCheckData.url.trim() > 0 ? originalCheckData.url.trim() : null;
-//   originalCheckData.method = typeof(originalCheckData.method) ===`string` && [`post`, `get`, `put`, `delete`].indexOf(originalCheckData.method) > -1 ? originalCheckData.method : null;
-//   originalCheckData.successCodes = typeof(originalCheckData.successCodes) ===`object` && originalCheckData.successCodes instanceof Array && originalCheckData.successCodes.length > 0 ? originalCheckData.successCodes : null;
-//   originalCheckData.timeoutSeconds = typeof(originalCheckData.timeoutSeconds) ===`number` && originalCheckData.timeoutSeconds % 1 === 0 && originalCheckData.timeoutSeconds >= 1 && originalCheckData.timeoutSeconds <= 5 ? originalCheckData.timeoutSeconds : null;
-//
-//   // Set the keys that may not be set if the workers have not run this check before
-//   originalCheckData.state = typeof(originalCheckData.state) ===`string` && [`up`, `down`].indexOf(originalCheckData.state) > -1 ? originalCheckData.state : `down`;
-//   originalCheckData.lastChecked = typeof(originalCheckData.lastChecked) ===`number` && originalCheckData.lastChecked > 0 ? originalCheckData.lastChecked : null;
-//
-//   // If all the checks pass, pass this data on to the next step in the process
-//   if (originalCheckData.id
-//       && originalCheckData.userPhone
-//       && originalCheckData.protocol
-//       && originalCheckData.url
-//       && originalCheckData.successCodes
-//       && originalCheckData.timeoutSeconds) {
-//     workers.performCheck(originalCheckData);
-//   } else {
-//     console.log(`Error: Check ID ${originalCheckData.id} is not properly formatted. Skipping it.`);
-//   }
-// };
 
 
 // Sanity-check the check data
@@ -94,7 +67,7 @@ workers.validateCheckData = (originalCheckData) => {
     workers.performCheck(originalCheckData);
   } else {
     // If checks fail, log the error and fail silently
-    console.log("Error: one of the checks is not properly formatted. Skipping.");
+    debug("Error: one of the checks is not properly formatted. Skipping.");
   }
 };
 
@@ -196,10 +169,10 @@ workers.processCheckOutcome = (originalCheckData, checkOutcome) => {
       if (alertWarranted) {
         workers.alertUserToStatusChange(newCheckData);
       } else {
-        console.log(`Check outcome has not changed, no alert needed`);
+        debug(`Check outcome has not changed, no alert needed`);
       }
     } else {
-      console.log(`Error trying to save updates to one of the checks: ${newCheckData.id}`);
+      debug(`Error trying to save updates to one of the checks: ${newCheckData.id}`);
     }
   });
 
@@ -211,9 +184,9 @@ workers.alertUserToStatusChange = (newCheckData) => {
   let msg = `Alert: Your check for ${newCheckData.method.toUpperCase()} ${newCheckData.protocol}://${newCheckData.url} is currently ${newCheckData.state}`;
   helpers.sendTwilioSMS(newCheckData.userPhone, msg, (err) => {
     if (!err) {
-      console.log(`Success: User was alerted to a change in their check, via SMS: ${msg}`);
+      debug(`Success: User was alerted to a change in their check, via SMS: ${msg}`);
     } else {
-      console.log(`Error: Could not send SMS alert to user who had a state check: ${msg}`);
+      debug(`Error: Could not send SMS alert to user who had a state check: ${msg}`);
     }
   });
 }
@@ -238,9 +211,9 @@ workers.log = (originalCheckData, checkOutcome, state, alertWarranted, timeOfChe
   // Append the log string to the file
   _logs.append(logFileName, logStr, (err) => {
     if (!err) {
-      console.log(`Success: logged to file.`);
+      debug(`Success: logged to file.`);
     } else {
-      console.log(`Error: logging to file failed.`);
+      debug(`Error: logging to file failed.`);
 
     }
   });
@@ -270,18 +243,18 @@ workers.rotateLogs = () => {
             // Truncate the log
             _logs.truncate(logId, (err) => {
               if (!err) {
-                console.log(`Success truncating the log file`);
+                debug(`Success truncating the log file`);
               } else {
-                console.log(`Error truncating the log file: ${logId}, ${err}`);
+                debug(`Error truncating the log file: ${logId}, ${err}`);
               }
             });
           } else {
-            console.log(`Error: could not compress one of the log files: ${logId}, ${err}`);
+            debug(`Error: could not compress one of the log files: ${logId}, ${err}`);
           }
         });
       });
     } else {
-      console.log(`Error: could not find any logs to rotate`);
+      debug(`Error: could not find any logs to rotate`);
     }
   });
 }
@@ -297,6 +270,9 @@ workers.logRotationLoop = () => {
 
 // Init workers
 workers.init = () => {
+  // Send to console, in yellow
+  console.log(`\x1b[33m%s\x1b[0m`, `Background workers are running`);
+
   // Execute all of the checks immediately
   workers.gatherAllChecks();
 
